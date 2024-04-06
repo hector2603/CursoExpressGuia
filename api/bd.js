54f9886dd6f4f072c1aab98f2ebbfcd4d717b58e
Héctor Ocampo <hector2603@users.noreply.github.com>
2024-04-06 18:14:45 -0500

    Update bd.js

--- api/bd.js
+++ api/bd.js
@@ -1,4 +1,6 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
+const pg = require('pg')
+
 const sequelize = new Sequelize("verceldb", "default", "dcxGX3ZwO5vo", {
   host: "ep-lingering-bread-a4e1d0fw-pooler.us-east-1.aws.neon.tech",
   dialect: "postgres",
